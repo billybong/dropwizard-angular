7c19593be7928dc4ad00f917742a7674626424a3
Billy Sjöberg <billy.sjoberg@eyc.com>
2013-11-14 17:12:16 +0100

    added favicon

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -7,7 +7,6 @@ var employeeApp = angular.module('employeeApp',[
         $routeProvider.
             when('/', {templateUrl: 'partials/employees-list.html', controller: 'EmployeesListCtrl'}).
             when('/employees/:employeeId', {templateUrl: '/partials/employee-details.html', controller: 'EmployeeDetailCtrl'}).
-            //when('/employees/:employeeId/edit', {templateUrl: 'partials/employee-edit.html', controller: EmployeeeEditCtrl}).
             //when('/employees/:employeeId/new', {templateUrl: 'partials/employee-new.html', controller: EmployeeNewCtrl}).
             otherwise({redirectTo: '/'});
 
--- src/main/resources/assets/js/controllers.js
+++ src/main/resources/assets/js/controllers.js
@@ -1,14 +1,24 @@
-angular.module('employeeApp.controllers',[]).
-    controller('EmployeesListCtrl',['$scope', 'EmployeeFactory', function($scope, EmployeeFactory) {
-        $scope.data = {employees: EmployeeFactory.query()};
-    }]).
-    controller('EmployeeDetailCtrl', ['$scope', '$routeParams', 'EmployeeFactory', '$location', function($scope, $routeParams, EmployeeFactory, $location){
-        $scope.data = {
-            employee: EmployeeFactory.get({id:$routeParams.employeeId})
-        };
+var mod = angular.module('employeeApp.controllers', []);
 
-        $scope.update = function(){
-            EmployeeFactory.update($scope.data.employee)
-            $location.path('/');
-        }
-    }]);
+mod.controller('EmployeesListCtrl', ['$scope', 'EmployeeFactory', function ($scope, EmployeeFactory) {
+    $scope.data = {employees: EmployeeFactory.query()};
+}]);
+
+
+mod.controller('FakeCtrl', ['$scope', function ($scope) {
+    $scope.data = {employees: [
+        {id: 1, name: 'Dumbo', yearsWorked:5, skill:'none'},
+        {id: 2, name: 'Långben', yearsWorked:2, skill:'peta näsan'}
+    ]};
+}]);
+
+mod.controller('EmployeeDetailCtrl', ['$scope', '$routeParams', 'EmployeeFactory', '$location', function ($scope, $routeParams, EmployeeFactory, $location) {
+    $scope.data = {
+        employee: EmployeeFactory.get({id: $routeParams.employeeId})
+    };
+
+    $scope.update = function (employee) {
+        EmployeeFactory.update(employee)
+        $location.path('/');
+    }
+}]);
