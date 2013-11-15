da5239168eb12b324109d2257f90a5a0728c3ed9
Billy Sjöberg <billy.sjoberg@eyc.com>
2013-11-15 16:34:27 +0100

    added a  version of the service as well to lab with backends without

--- src/main/resources/assets/js/controllers.js
+++ src/main/resources/assets/js/controllers.js
@@ -1,24 +1,42 @@
 var mod = angular.module('employeeApp.controllers', []);
 
-mod.controller('EmployeesListCtrl', ['$scope', 'EmployeeFactory', function ($scope, EmployeeFactory) {
-    $scope.data = {employees: EmployeeFactory.query()};
+/**********************************************
+ * Controllers wired to Factories using the $resource service
+ **********************************************/
+
+mod.controller('EmployeesListCtrlUsingResource', ['$scope', 'EmployeeFactory', function ($scope, EmployeeFactory) {
+    $scope.data = {employees: EmployeeFactoryUsingResource.query()};
 }]);
 
-
-mod.controller('FakeCtrl', ['$scope', function ($scope) {
-    $scope.data = {employees: [
-        {id: 1, name: 'Dumbo', yearsWorked:5, skill:'none'},
-        {id: 2, name: 'Långben', yearsWorked:2, skill:'peta näsan'}
-    ]};
-}]);
-
-mod.controller('EmployeeDetailCtrl', ['$scope', '$routeParams', 'EmployeeFactory', '$location', function ($scope, $routeParams, EmployeeFactory, $location) {
+mod.controller('EmployeeDetailCtrlUsingResource', ['$scope', '$routeParams', 'EmployeeFactory', '$location', function ($scope, $routeParams, EmployeeFactory, $location) {
     $scope.data = {
-        employee: EmployeeFactory.get({id: $routeParams.employeeId})
+        employee: EmployeeFactoryUsingResource.get({id: $routeParams.employeeId})
     };
 
     $scope.update = function (employee) {
         EmployeeFactory.update(employee)
         $location.path('/');
+    }
+}]);
+
+/**********************************************************
+ * Controllers wired to Factories using a regular $http service
+ **********************************************************/
+
+mod.controller('EmployeesListCtrl', ['$scope', 'EmployeeFactory', function ($scope, EmployeeFactory) {
+    EmployeeFactory.query(function(data){
+        $scope.data = {employees: data};
+    });
+}]);
+
+mod.controller('EmployeeDetailCtrl', ['$scope', '$routeParams', 'EmployeeFactory', '$location', function ($scope, $routeParams, EmployeeFactory, $location) {
+    EmployeeFactory.get({id: $routeParams.employeeId}, function(data){
+        $scope.data = { employee: data};
+    });
+
+    $scope.update = function (employee) {
+        EmployeeFactory.update(employee, function(){
+            $location.path('/');
+        })
     }
 }]);
--- src/main/resources/assets/js/services.js
+++ src/main/resources/assets/js/services.js
@@ -1,7 +1,33 @@
-angular.module('employeeApp.services', ['ngResource'])
-    .factory('EmployeeFactory', function($resource){
+var module = angular.module('employeeApp.services', ['ngResource']);
+
+/***************************************************************
+ * Using $resource
+ ***************************************************************/
+
+module.factory('EmployeeFactoryUsingResource', function($resource){
         return $resource('/api/employees/:id',{id:'@id'}, {
             update: {method: 'PUT'}
         });
+    })
+    .value('version', '0.1');
+
+/***************************************************************
+ * Using a simple $http
+ ***************************************************************/
+
+module.factory('EmployeeFactory', function($http){
+        return {
+            'update': function(employee, callback){
+                $http.put('/api/employees/' + employee.id, employee).success(callback);
+            },
+
+            'query': function(callback){
+                $http.get('/api/employees').success(callback);
+            },
+
+            'get': function(employee, callback){
+                $http.get('/api/employees/' + employee.id).success(callback);
+            }
+        };
     })
     .value('version', '0.1');
